73569468f9f1d080e6d25de25d49d1b6e2273d2e
Makktu <johnmcna@gmail.com>
2021-08-10 19:35:37 +0100

    Basic setup Tuesday

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 "use strict";
+
+// *****************************************
+// ************** FUNCTIONS ****************
+// *****************************************
+
+function openOrCloseForm(e) {
+    newForm.classList.toggle("invisible");
+
+    if (!newForm.classList.contains("invisible")) {
+        newBtn.textContent = "✖️";
+        newBtn.style.backgroundColor = "red";
+    } else {
+        newBtn.textContent = "➕";
+        newBtn.style.backgroundColor = "turquoise";
+    }
+}
+
+function saveClicked() {
+    console.log("Save clicked");
+}
+
+function clearClicked() {
+    console.log("Clear clicked");
+}
+
+// *****************************************
+// ********* ASSIGN BUTTON CONTROLS ********
+// *****************************************
+
+const newBtn = document.querySelector(".new-button");
+newBtn.addEventListener("click", (e) => {
+    openOrCloseForm(e);
+});
+
+const newForm = document.querySelector(".new-form");
+
+const saveBtn = document.querySelector(".save-btn");
+const clearBtn = document.querySelector(".clear-btn");
+
+saveBtn.addEventListener("click", saveClicked);
+clearBtn.addEventListener("click", clearClicked);
+
+const messageArea = document.querySelector(".message-area");
+
+let theLibrary = [];
+
+messageArea.innerHTML = `<p>${theLibrary.length} books in the library</p>`;
